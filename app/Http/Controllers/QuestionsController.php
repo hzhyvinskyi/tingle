4c7958076efa95c5db469bdafe091f8c94c8d6d2
hzhyvinskyi <hzhyvinskyi@gmail.com>
2018-11-04 00:09:11 -0700

    Authorization implementation using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Gate;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -66,9 +71,11 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        if (Gate::denies('update-question', $question)) {
-            abort(403);
-        }
+//        if (Gate::denies('update-question', $question)) {
+//            abort(403);
+//        }
+
+        $this->authorize('update', $question);
 
         return view('questions.edit', compact('question'));
     }
@@ -82,9 +89,11 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
-        if (Gate::denies('update-question', $question)) {
-            abort(403);
-        }
+//        if (Gate::denies('update-question', $question)) {
+//            abort(403);
+//        }
+
+        $this->authorize('update', $question);
 
         $question->update($request->only('title', 'text'));
 
@@ -100,9 +109,11 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        if (Gate::denies('delete-question', $question)) {
-            abort(403);
-        }
+//        if (Gate::denies('delete-question', $question)) {
+//            abort(403);
+//        }
+
+        $this->authorize('delete', $question);
 
         $question->delete();
 
